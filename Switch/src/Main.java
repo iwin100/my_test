82407dd565ff822ab554f32337bc45ff5c3a2b01
j <j@j-PC>
2017-11-01 00:59:07 +0900

    PASS

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -7,6 +7,7 @@ import java.util.StringTokenizer;
 // https://www.acmicpc.net/problem/1395
 public class Main {
 	static int tree[];
+	static int lazy[];
 	static int info[];
 	public static void main(String[] args) throws IOException {
 		System.setIn(new FileInputStream("input"));
@@ -19,7 +20,7 @@ public class Main {
 		int M = Integer.parseInt(token.nextToken());
 		
 		tree = new int[N*4];
-		info = new int[N+1];
+		lazy = new int[N*4];
 		
 		while (M-- > 0) {
 			token = new StringTokenizer(br.readLine());
@@ -39,64 +40,64 @@ public class Main {
 	}
 	
 	private static void range_update(int idx, int start, int end, int left, int right) {
+		update_lazy(idx, start, end);
+		
 		if (right < start || left > end) {
 			return;
 		}
 		
 		// 리프 노드
 		if (start == end) {
-			if (info[end] == 0) {
-				tree[idx]++;
-				info[end]++;
+			if (tree[idx] == 0) {
+				tree[idx] = 1;
 			} else {
-				tree[idx]--;
-				info[end]--;
+				tree[idx] = 0;
 			} 
 			return;
 		}
 		
-		// 값 구하기
-//		tree[idx] += getDiff(start, end, left, right);
-		
-		range_update(idx*2, start, (start+end)/2, left, right);
-		range_update(idx*2+1, (start+end)/2+1, end, left, right);
-		tree[idx] = tree[idx*2] + tree[idx*2+1];
-	}
-
-
-	private static int getDiff(int start, int end, int left, int right) {
-		int diff = 0;
-		if (left >= start && right <= end) {
-			for (int i = left; i<=right; i++) {
-				if (info[i] == 0) {
-					diff++;	
-				} else {
-					diff--;
-				}
+		if (start >= left && end <= right) {
+			//lazy
+			tree[idx] = (end-start+1) - tree[idx];
+			if (lazy[idx*2] == 0) {
+				lazy[idx*2] = 1;
+			} else {
+				lazy[idx*2] = 0;
 			}
-		} else if (left >= start && right > end) {
-			for (int i = left; i<=end; i++) {
-				if (info[i] == 0) {
-					diff++;	
-				} else {
-					diff--;
-				}
+			if (lazy[idx*2+1] == 0) {
+				lazy[idx*2+1] = 1;
+			} else {
+				lazy[idx*2+1] = 0;
 			}
-		} else if (left > start && right <= end) {
-			for (int i = start; i<=end; i++) {
-				if (info[i] == 0) {
-					diff++;	
-				} else {
-					diff--;
-				}
-			}
+		} else {
+			range_update(idx*2, start, (start+end)/2, left, right);
+			range_update(idx*2+1, (start+end)/2+1, end, left, right);
+			tree[idx] = tree[idx*2] + tree[idx*2+1];
 		}
-		return diff;
 	}
-
-
+	
+	private static void update_lazy(int idx, int start, int end) {
+		if (lazy[idx] == 1) {
+			tree[idx] = (end-start+1) - tree[idx];
+			
+			if (start != end) {
+				if (lazy[idx*2] == 0) {
+					lazy[idx*2] = 1;
+				} else {
+					lazy[idx*2] = 0;
+				}
+				if (lazy[idx*2+1] == 0) {
+					lazy[idx*2+1] = 1;
+				} else {
+					lazy[idx*2+1] = 0;
+				}
+			}
+			lazy[idx] = 0;
+		}
+	}
 
 	private static int get(int idx, int start, int end, int left, int right) {
+		update_lazy(idx, start, end);
 		if (right < start || left > end) {
 			return 0;
 		}
@@ -106,24 +107,5 @@ public class Main {
 		}
 		
 		return get(idx*2, start, (start+end)/2, left, right) + get(idx*2+1, (start+end)/2+1, end, left, right);
-	}
-	
-	private static void update(int idx, int start, int end, int target) {
-		if (target < start || target > end) {
-			return;
-		}
-		
-		if (info[target] == 0) {
-			tree[idx]++;
-		} else {
-			tree[idx]--;
-		}
-		
-		if (start == end) {
-			return;
-		}
-		
-		update(idx*2, start, (start+end)/2, target);
-		update(idx*2+1, (start+end)/2+1, end, target);
 	}
 }
