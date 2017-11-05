c0470c356c02487cb921ee48b071bed58b6eb501
j <j@j-PC>
2017-11-06 00:56:55 +0900

    ... ing

--- GoldMine/src/Main.java
+++ GoldMine/src/Main.java
@@ -2,14 +2,17 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/10167
 public class Main {
 	static Mine mine[];
-	static int table[][];
+	static int map[];
+	static ArrayList<Mine> mapList;
 	static long big;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -20,7 +23,6 @@ public class Main {
 		
 		int N = Integer.parseInt(br.readLine());
 		mine = new Mine[N];		
-		int idx = 0;
 		
 		for (int i = 0; i<N; i++) {
 			token = new StringTokenizer(br.readLine());
@@ -29,11 +31,9 @@ public class Main {
 			int z = Integer.parseInt(token.nextToken());
 			
 			mine[i] = new Mine(x,y,z);
-			if (z > 0) {
-				idx++;
-			}
 		}
 		
+		// x축 기준으로 정렬
 		Arrays.sort(mine, new Comparator<Mine>() {
 			@Override
 			public int compare(Mine o1, Mine o2) {
@@ -47,21 +47,59 @@ public class Main {
 			}
 		});
 		
-		table = new int[idx+1][idx+1];
-		for (int i = 0; i<N; i++) {
-			if (mine[i].x > 0) {
-				table[0][i] = mine[i-1].x;
-				table[i][0] = mine[i-1].x;
-				table[i][i] = mine[i-1].x;
+		for (int i = 0; i< N; i++) {
+			mapList = new ArrayList<>();
+			for (int j = 0; j <= i; j++) {
+				insert(mine[j]);
+			}
+			if (i+1<N && mine[i].x == mine[i+1].x) {
+				continue;
+			} else {
+				getBig();
 			}
 		}
 		
-		
-		for (int i = 2; i <= idx; i++) {
-			
+		System.out.println(big);
+	}
+
+	private static void insert(Mine mine) {
+		int size = mapList.size();
+		if (size == 0) {
+			mapList.add(mine);
+			return;
 		}
-		
-		System.out.println(mine[0]);
+		int i;
+		for (i = 0; i < size; i++) {
+			if (mapList.get(i).y == mine.y) {
+				mapList.remove(i);
+				mapList.add(i, mine);
+				return;
+			} else if (mapList.get(i).y < mine.y) {
+				mapList.add(i, mine);
+				return;
+			} else {	
+			}
+		}
+		mapList.add(mine);
+		return;
+	}
+
+	private static void getBig() {
+		int size = mapList.size();
+		if (size == 1) {
+			big = mapList.get(0).value;
+			return;
+		} else {
+			long bigbig = mapList.get(0).value;
+			for (int i = 1; i<size; i++) {
+				if (bigbig + mapList.get(i).value > mapList.get(i).value) {
+					bigbig += mapList.get(i).value;
+				} else {
+					bigbig = mapList.get(i).value;
+				}
+				big = Math.max(bigbig, big);
+			}	
+		}
 	}
 }
 
