3d1d35d7c61f842f15c116585a7f1fe2c85c9b6a
jh <jonghwan2.park@gmail.com>
2017-11-23 23:54:21 +0900

    fail...

--- ColoringTheTree/src/Main.java
+++ ColoringTheTree/src/Main.java
@@ -3,13 +3,11 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/1693
 public class Main {
 	static ArrayList<Node> input = new ArrayList<>();
-	static boolean childColor[] = new boolean[100001];
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		System.setIn(new FileInputStream("input"));
 		InputStreamReader isr = new InputStreamReader(System.in);
@@ -27,48 +25,57 @@ public class Main {
 			input.get(src).connect.add(input.get(dst));
 			input.get(dst).connect.add(input.get(src));
 		}
+		
 		dfs(0);
-		System.out.println(input.get(0).sum);
+		int c_table[] = input.get(0).table;
+		System.out.println(Math.min(Math.min(c_table[0], c_table[1]),
+				                    Math.min(c_table[2], c_table[3])
+				                   )
+				          );
 	}
 	
 	private static void dfs(int idx) {
 		input.get(idx).visit = true;
+		input.get(idx).parent = true;
 		for (int i = 0; i< input.get(idx).connect.size(); i++) {
 			if (input.get(idx).connect.get(i).visit == true) {
 				continue;
 			}
 			dfs(input.get(idx).connect.get(i).idx);
 		}
+		input.get(idx).parent = false;
 		
-		// get child color
-		Arrays.fill(childColor, false);
 		for (int i = 0; i< input.get(idx).connect.size(); i++) {
-			if (input.get(idx).connect.get(i).color == -1) {
+			Node child = input.get(idx).connect.get(i);
+			if (child.parent == true) {
 				continue;
 			}
-			int color = input.get(idx).connect.get(i).color;
-			childColor[color] = true;
-			input.get(idx).sum += input.get(idx).connect.get(i).sum;
-		}
-		
-		for (int i = 1; i<=100000; i++) {
-			if (childColor[i] != true) {
-				input.get(idx).color = i;
-				input.get(idx).sum += i;
-				break;
-			}
+			
+			int c_table[] = child.table;
+			input.get(idx).table[0] += Math.min(c_table[1], 
+					                            Math.min(c_table[2], c_table[3])); 
+			input.get(idx).table[1] += Math.min(c_table[0],
+					                            Math.min(c_table[2], c_table[3]));
+			input.get(idx).table[2] += Math.min(c_table[0],
+					                            Math.min(c_table[1], c_table[3]));
+			input.get(idx).table[3] += Math.min(c_table[0],
+					                            Math.min(c_table[1], c_table[2]));
 		}
 	}
 }
 
 class Node {
 	ArrayList<Node> connect = new ArrayList<>();
+	int table[] = new int[4];
 	int idx;
-	int color = -1;
-	long sum = 0;
 	boolean visit = false;
+	boolean parent = false;
 	
 	Node(int idx){
 		this.idx = idx;
+		this.table[0] = 1;
+		this.table[1] = 2;
+		this.table[2] = 3;
+		this.table[3] = 4;
 	}
 }
